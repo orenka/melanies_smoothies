2cae90e2afcf5f1deee67595af1caf4c5169dc2c
Oren Katznelson <36994559+orenka@users.noreply.github.com>
2025-09-09 10:04:04 -0400

    Get nutrition info and display in dataframes for all fruits chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,10 @@ if ingredients_list: # MEANS IF THIS LIST IS NOT NULL
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'nutrition information:')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
+
     st.write (ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -39,7 +43,3 @@ if ingredients_list: # MEANS IF THIS LIST IS NOT NULL
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# Display SmoothieFroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
