693faa3955fc7fa63c9a1798efa15f16058bf483
Oren Katznelson <36994559+orenka@users.noreply.github.com>
2025-09-09 09:51:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,5 @@ if ingredients_list: # MEANS IF THIS LIST IS NOT NULL
 
 # Display SmoothieFroot nutrition information
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
